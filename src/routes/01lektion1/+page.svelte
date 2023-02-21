a269938ca3fca6e76b4e149c1155046d258d321a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 14:05:11 +0100

    feat: :sparkles: add 01lektion1 route
    should be layout later

---
+++ src/routes/01lektion1/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from "$app/stores";
+
+	let base = "01lektion1";
+	let routes = ["/01", "/exercise"];
+
+  // TODO fix base getting added twice
+</script>
+
+<header class="flex">
+	{#each routes as link}
+		<a class="m-4 rounded-lg bg-green-500 p-4" href="{base}{link}">{link}</a>
+	{/each}
+</header>
+
+<slot />
+
+<style>
+</style>
