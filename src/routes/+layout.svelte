d9f629c8c71449876c1fe50f84c0077875359e97
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 14:04:12 +0100

    feat: :sparkles: add layout routes as links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import "@unocss/reset/antfu.css";
+	let routes = ["/", "/01lektion1"];
 </script>
+
+<header class="flex">
+	{#each routes as link}
+		<a class="m-4 rounded-lg bg-blue-500 p-4" href={link}>{link}</a>
+	{/each}
+</header>
 
 <slot />
 
