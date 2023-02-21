eb03b428092755105844cb2512d54d0692dced79
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 14:03:03 +0100

    feat: :wrench: add presetForms()

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "unocss/vite";
 import presetUno from "@unocss/preset-uno";
+import { presetForms } from "@julr/unocss-preset-forms";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		UnoCSS({
-			presets: [presetUno()],
+			presets: [presetUno(), presetForms()],
 			mode: "svelte-scoped",
 		}),
 		sveltekit(),
