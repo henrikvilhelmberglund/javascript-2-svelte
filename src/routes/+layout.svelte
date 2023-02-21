8596dbffe333b8d0f3b4124e9fd9b91ab53bda96
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 15:58:45 +0100

    feat: :sparkles: show Nav only on +page.svelte in the index route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,6 @@
 <script>
 	import "@unocss/reset/antfu.css";
-	let routes = ["/", "/01lektion1"];
 </script>
-
-<header class="flex">
-	{#each routes as link}
-		<a class="m-4 rounded-lg bg-blue-500 p-4" href={link}>{link}</a>
-	{/each}
-</header>
 
 <slot />
 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01lektion1"];
+	let prefix = "/01lektion1";
+</script>
+
+<Nav {routes} {prefix} />
