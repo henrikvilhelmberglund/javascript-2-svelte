046fb14ed0b25dcea297b8b797962f5f29c7f06c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 15:02:28 +0100

    fix: :bug: don't clear body innerHTML

--- src/routes/01lektion1/01/+page.svelte
+++ src/routes/01lektion1/01/+page.svelte
@@ -19,7 +19,7 @@
 	onMount(() => {
 		let body = document.querySelector("body");
 		let div = document.createElement("div");
-		body.innerHTML = "";
+		// body.innerHTML = "";
 
 		let sortedHeroes = [...data];
 
