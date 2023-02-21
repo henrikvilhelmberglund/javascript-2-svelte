11685ca2cce3237ed7243a627e9468280f738ce5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-21 15:59:14 +0100

    fix: :fire: remove old header

--- src/routes/01lektion1/+page.svelte
+++ src/routes/01lektion1/+page.svelte
@@ -1,19 +1,5 @@
 <script>
-	import { page } from "$app/stores";
-
-	let base = "01lektion1";
-	let routes = ["/01", "/exercise", "/02"];
-
-  // TODO fix base getting added twice
 </script>
-
-<header class="flex">
-	{#each routes as link}
-		<a class="m-4 rounded-lg bg-green-500 p-4" href="{base}{link}">{link}</a>
-	{/each}
-</header>
-
-<slot />
 
 <style>
 </style>
