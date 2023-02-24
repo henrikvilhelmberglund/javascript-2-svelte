c210cf30a8c0baa2140a6ad13ed987d164fd6c60
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-24 14:17:38 +0100

    feat: :sparkles: update 02

--- src/routes/02lektion2/02/+page.svelte
+++ src/routes/02lektion2/02/+page.svelte
@@ -1,11 +1,81 @@
 <script>
-  
+	class Person {
+		constructor(name, age) {
+			this.name = name;
+			this.age = age;
+		}
+		greet() {
+			console.log(`Hi! My name is ${this.name} and I am ${this.age}`);
+			return `Hi! My name is ${this.name} and I am ${this.age}`;
+		}
+	}
+
+	class Student extends Person {
+		// behöver inte vara samma som i Person
+		constructor(name, age, grade) {
+			// this.name = name;
+			// this.age = age;
+			super(name, age);
+			this.averageGrade = grade;
+		}
+		greet() {
+			let greet = super.greet();
+			return `${greet} and a student!`;
+		}
+    welcome() {
+			// let welcome = super.greet();
+			return `Welcome to Ankademin! 🦆`;
+		}
+	}
+
+	class StudentRepresentative extends Student {
+		constructor(name, age, grade, tenure) {
+			super(name, age, grade);
+			this.tenure = tenure;
+		}
+		greet() {
+			let greet = super.greet();
+			let welcome = super.welcome();
+			return `${greet} ${welcome} By the way, I rule this classroom 😄`;
+		}
+	}
+
+	class Teacher extends Person {
+		constructor(name, age, students) {
+			super(name, age);
+			this.students = students;
+		}
+	}
+
+	let person1 = new Person("Matt", 34);
+	let student1 = new Student("Nala", 16, 6.7);
+	let teacher1 = new Teacher("Batman", 40, 37);
+	let studentRep = new StudentRepresentative("Kalle", 35, 9, "6 månader");
+
+  console.log(student1)
 </script>
 
-<main class="p-4">
-  hi
+<main class="flex flex-col p-4">
+	<p>
+		{JSON.stringify(person1)}
+	</p>
+	<p>
+		{JSON.stringify(student1)}
+	</p>
+	<p>
+		{JSON.stringify(teacher1)}
+	</p>
+  <p>
+    {student1.greet()}
+  </p>
+  <p>
+    {student1.welcome()}
+  </p>
+	<p>
+		{JSON.stringify(studentRep)}
+	</p>
+	{studentRep.greet()}
 </main>
 
 <style>
-  
 </style>
