7cb257bc1ea45ea5f06166fe0c89807d7a8b6992
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-24 10:31:47 +0100

    feat: :sparkles: add 01 route

---
+++ src/routes/02lektion2/01/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+let date = new Date();
+let day = date.getDay();
+
+</script>
+
+<main class="p-4">
+  <p>
+    hello world
+  </p>
+  <p>
+    today is {day} ??
+  </p>
+</main>
+
+<style>
+</style>
