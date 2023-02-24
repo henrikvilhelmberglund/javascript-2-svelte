5c45e9516b657430d8d08f1dbe42506e593fddcc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-24 15:12:44 +0100

    feat: :sparkles: update 02

--- src/routes/02lektion2/02/+page.svelte
+++ src/routes/02lektion2/02/+page.svelte
@@ -26,7 +26,22 @@
 			// let welcome = super.greet();
 			return `Welcome to Ankademin! 🦆`;
 		}
+    static compareGrades(student1, student2) {
+      if(student1.averageGrade>student2.averageGrade) {
+        return `${student1.name} has better grades than ${student2.name}`;
+      }
+      else if (student1.averageGrade<student2.averageGrade) {
+        return `${student2.name} has better grades than ${student1.name}`;
+      }
+    }
 	}
+
+  class Student2 extends Person {
+    // om man skippar constructor tas allt från parent
+    hi() {
+      return "Hi! I am a student2!"
+    }
+  }
 
 	class StudentRepresentative extends Student {
 		constructor(name, age, grade, tenure) {
@@ -45,10 +60,21 @@
 			super(name, age);
 			this.students = students;
 		}
+    feedback(student) {
+      if(student.averageGrade<5) {
+        return `You need to step it up ${student.name}`
+      }
+      else {
+        return `You're doing good! Keep up the good work ${student.name}!`
+      }
+    }
 	}
 
 	let person1 = new Person("Matt", 34);
 	let student1 = new Student("Nala", 16, 6.7);
+	let student2 = new Student2("Nala", 16, 3);
+	let student3 = new Student("Superman", 16, 3);
+	let student4 = new Student("Dio", 16, 9);
 	let teacher1 = new Teacher("Batman", 40, 37);
 	let studentRep = new StudentRepresentative("Kalle", 35, 9, "6 månader");
 
@@ -63,6 +89,9 @@
 		{JSON.stringify(student1)}
 	</p>
 	<p>
+		{JSON.stringify(student2.hi())}
+	</p>
+	<p>
 		{JSON.stringify(teacher1)}
 	</p>
   <p>
@@ -74,7 +103,22 @@
 	<p>
 		{JSON.stringify(studentRep)}
 	</p>
-	{studentRep.greet()}
+  <p>
+    {studentRep.greet()}
+  </p>
+  <p>
+    {teacher1.feedback(student1)}
+  </p>
+  <p>
+    {teacher1.feedback(student3)}
+  </p>
+  <p>
+    <!-- {student1.compareGrades()} fungerar inte pga static -->
+    {Student.compareGrades(student1, student3)}
+  </p>
+  <p>
+    {Student.compareGrades(student4, student1)}
+  </p>
 </main>
 
 <style>
