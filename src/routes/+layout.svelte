b1634d1e577d5b9f04098b206a1c85fb8e88d8c8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-24 15:36:58 +0100

    fix: :recycle: move Nav to layout instead of page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import "@unocss/reset/antfu.css";
+	import Nav from "$lib/Nav.svelte";
+  import { base } from "$app/paths";
+	let routes = ["01lektion1", "02lektion2"];
+	let prefix = "/";
+  // TODO fix empty thingy
 </script>
+
+<Nav {routes} {prefix} />
 
 <slot />
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-	import Nav from "$lib/Nav.svelte";
-	let routes = ["01lektion1", "02lektion2"];
-	let prefix = "";
-  // TODO fix empty thingy
+  
 </script>
 
-<Nav {routes} {prefix} />
+
+
+<style>
+  
+</style>
