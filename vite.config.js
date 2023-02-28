937db7cb6b50deec1a5f2385437e2ccd29b906ee
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-28 12:19:33 +0100

    feat: :wrench: add transformerDirectives to config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import UnoCSS from "unocss/vite";
 import presetUno from "@unocss/preset-uno";
 import { presetForms } from "@julr/unocss-preset-forms";
 import { defineConfig } from "vite";
+import transformerDirectives from "@unocss/transformer-directives";
+
 
 export default defineConfig({
 	plugins: [
-		UnoCSS({
+    UnoCSS({
+      transformers: [transformerDirectives()],
 			presets: [presetUno(), presetForms()],
 			mode: "svelte-scoped",
 		}),
