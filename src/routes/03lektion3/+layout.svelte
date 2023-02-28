24a1a2ed081f776b8f708020306ab1d98209a028
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-28 10:27:31 +0100

    feat: :sparkles: add new layouts

---
+++ src/routes/03lektion3/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01", "exercise", "02"];
+	let prefix = "/03lektion3";
+</script>
+
+<Nav {routes} {prefix} />
+<slot />
+
+<style>
+</style>
