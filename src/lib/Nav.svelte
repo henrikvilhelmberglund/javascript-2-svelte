dd9edefc3db3427c25a91d656af730153d676485
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-28 10:27:01 +0100

    fix: :lipstick: make nav smaller

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -10,16 +10,16 @@
 <nav class="flex flex-col">
 	<div class="flex-row [&>*]:m-2">
 		{#if prefix === "/"}
-			<a class="rounded-lg bg-violet-400 p-4 text-black" href="{base}/">Home</a>
+			<a class="rounded-lg bg-violet-400 px-4 text-black" href="{base}/">Home</a>
 			{#each routes as route}
-				<a class="rounded-lg bg-violet-400 p-4 text-black" href="{base}/{route}">{route}</a>
+				<a class="rounded-lg bg-violet-400 px-4 text-black" href="{base}/{route}">{route}</a>
 			{/each}
 		{/if}
 	</div>
 	<div class="flex-row [&>*]:m-2">
 		{#each routes || [] as route}
 			{#if prefix !== "/"}
-				<a class="rounded-lg bg-blue-400 p-4 text-black" href="{base}{prefix}/{route}">{route}</a>
+				<a class="rounded-lg bg-blue-400 px-4 text-black" href="{base}{prefix}/{route}">{route}</a>
 			{/if}
 		{/each}
 	</div>
