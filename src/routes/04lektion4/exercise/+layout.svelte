0ac436ef612c6487b901101d36f1bcca4e9433ee
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-06 23:09:11 +0100

    fix: :bug: make layout into a real layout

---
+++ src/routes/04lektion4/exercise/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01", "02", "03"];
+	let prefix = "/04lektion4/exercise";
+</script>
+
+<Nav {routes} {prefix} />
+<slot />
+
+<style>
+</style>
--- src/routes/04lektion4/exercise/+page.svelte
+++ src/routes/04lektion4/exercise/+page.svelte
@@ -1,14 +1,1 @@
-<script>
-	import Nav from "$lib/Nav.svelte";
-	let routes = ["01", "02", "03"];
-	let prefix = "exercise";
-
-  // TODO fix nested layout
-
-</script>
-
-<!-- <Nav {routes} {prefix} /> -->
-<slot />
-
-<style>
-</style>
+<!-- TODO port JS solutions to Svelte -->
