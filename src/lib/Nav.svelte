420293c98912bd28006c23a37bb7a7be0f61d542
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-06 21:09:15 +0100

    fix: :bug: fix nested navs
    this is pretty stupid but I don't know of a better way right now

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,25 +1,29 @@
 <script>
 	export let routes;
 	export let prefix;
+	export let level;
 	import { page } from "$app/stores";
 	import { base } from "$app/paths";
 </script>
 
-<!-- {$page.route.id} -->
-
 <nav class="flex flex-col">
 	<div class="flex-row [&>*]:m-2">
-		{#if prefix === "/"}
-			<a class="rounded-lg bg-violet-400 px-4 text-black" href="{base}/">Home</a>
-			{#each routes as route}
-				<a class="rounded-lg bg-violet-400 px-4 text-black" href="{base}/{route}">{route}</a>
-			{/each}
+		{#if level === 1}
+			<a class="rounded-lg bg-purple-400 px-4 text-black" href="{base}/">Home</a>
 		{/if}
-	</div>
-	<div class="flex-row [&>*]:m-2">
-		{#each routes || [] as route}
-			{#if prefix !== "/"}
-				<a class="rounded-lg bg-blue-400 px-4 text-black" href="{base}{prefix}/{route}">{route}</a>
+		{#each routes as route}
+			{#if level === 1}
+				<a class="rounded-lg bg-purple-400 px-4 text-black" href="{base}{prefix}{route}"
+					>{route.replace("/", "")}</a>
+			{:else if level === 2}
+				<a class="rounded-lg bg-violet-400 px-4 text-black" href="{base}{prefix}{route}"
+					>{route.replace("/", "")}</a>
+			{:else if level === 3}
+				<a class="rounded-lg bg-indigo-400 px-4 text-black" href="{base}{prefix}{route}"
+					>{route.replace("/", "")}</a>
+			{:else if level === 4}
+				<a class="rounded-lg bg-blue-400 px-4 text-black" href="{base}{prefix}{route}"
+					>{route.replace("/", "")}</a>
 			{/if}
 		{/each}
 	</div>
