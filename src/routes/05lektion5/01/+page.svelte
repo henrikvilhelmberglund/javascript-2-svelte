a8bc748b84c6221fd93b9f4d95eb9a62b3e1cb08
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-07 10:52:12 +0100

    feat: :sparkles: add 05lektion5 01 route

--- src/routes/05lektion5/01/+page.svelte
+++ src/routes/05lektion5/01/+page.svelte
@@ -1,7 +1,39 @@
 <script>
+	// destructuring
+	//   let { name, email, address, company } = user;
+	//   let { street, city } = address;
+
+	// kan även byta namn på variabler
+	//   let { name: companyName, bs } = company;
+
+	// kan byta plats på query parameters
+	// /posts?id=12&userId=2
+	// eller
+	// /posts?userId=2&id=12
+
+	let getData = async () => {
+		let userId = 2;
+		let postId = 12;
+
+		let params = new URLSearchParams({
+			id: postId,
+			// istället för
+			// userId: userId
+			// fungerar i alla objekt
+			userId,
+		});
+
+		let data = await fetch(`https://jsonplaceholder.typicode.com/posts?${params}`);
+		let json = await data.json();
+		console.log(json);
+	};
+
+	getData();
 </script>
 
-<main class="p-4">hi</main>
+<main class="p-4">
+	<p class="text-[28rem]">🌞</p>
+</main>
 
 <style>
 </style>
